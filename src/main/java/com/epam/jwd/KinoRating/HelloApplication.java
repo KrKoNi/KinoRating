f2c269e758ead6b1e1d14155ac968d7a960b16be
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2020-12-04 17:03:13 +0300

    Project init

---
+++ src/main/java/com/epam/jwd/KinoRating/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.epam.jwd.KinoRating;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
