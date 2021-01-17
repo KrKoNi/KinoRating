7d206c5814ebe86fb934605536649a9949ea4513
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-17 23:44:41 +0300

    Read properties from file instead of hard-coding

--- src/main/java/com/epam/jwd/config/DatabaseProperties.java
+++ src/main/java/com/epam/jwd/config/DatabaseProperties.java
@@ -1,16 +1,37 @@
 package com.epam.jwd.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DatabaseProperties {
 
-    private static final String url = "jdbc:mysql://localhost:3306/kinorating";
-    private static final String user = "root";
-    private static final String password = "meowmeow";
+    private static String url;
+    private static String user;
+    private static String password;
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    static {
+        try (InputStream input = DatabaseProperties.class.getClassLoader().getResourceAsStream("database.properties")) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            url = prop.getProperty("db.url");
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error while reading properties");
         }
     }
 
