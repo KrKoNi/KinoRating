892b9c9f62bace8f2875a4230812401a74260ee9
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-17 23:49:45 +0300

    User DAO beautify and remove LastLogin for a while

--- src/main/java/com/epam/jwd/dao/impl/UserDAO.java
+++ src/main/java/com/epam/jwd/dao/impl/UserDAO.java
@@ -5,11 +5,16 @@ import com.epam.jwd.dao.DataAccessObject;
 import com.epam.jwd.domain.Role;
 import com.epam.jwd.domain.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserDAO implements DataAccessObject<User> {
 
@@ -21,6 +26,15 @@ public class UserDAO implements DataAccessObject<User> {
         return INSTANCE;
     }
 
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private LocalDate birthDate;
+    private String email;
+    private LocalDate registrationDate;
+    private int roleId;
 
     @Override
     public List<User> readAll() {
@@ -34,17 +48,15 @@ public class UserDAO implements DataAccessObject<User> {
                 int id = resultSet.getInt("id");
                 String login = resultSet.getString("login");
                 String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
                 String firstName = resultSet.getString("first_name");
                 String lastName = resultSet.getString("last_name");
                 LocalDate birthDate = resultSet.getDate("birth_date").toLocalDate();
                 LocalDate registrationDate = resultSet.getDate("registration_date").toLocalDate();
-                LocalDateTime lastLogin = LocalDateTime.now();//LocalDateTime.parse(resultSet.getString("last_login"));
                 int roleId = resultSet.getInt("role_id");
-                String roleName = resultSet.getString("role_name");
 
-
-                //User user = new User(id, login, password, firstName, lastName, birthDate, registrationDate, role);
-                //users.add(user);
+                User user = new User(id, login, password, firstName, lastName, birthDate, email, registrationDate, Role.getById(roleId));
+                users.add(user);
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -54,40 +66,36 @@ public class UserDAO implements DataAccessObject<User> {
         }
         return users;
     }
-    public User findByLoginAndPassword(String login, String password) {
-        Connection connection = BasicConnectionPool.getInstance().getConnection();
-        User user = null;
-        try (PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM kinorating.users where login = ? and password = ?"
-        )) {
-            statement.setString(1, login);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                LocalDate birthDate = resultSet.getDate("birth_date").toLocalDate();
-                String email = resultSet.getString("email");
-                LocalDate registrationDate = resultSet.getDate("registration_date").toLocalDate();
-                LocalDateTime lastLogin = LocalDateTime.now();//LocalDateTime.parse(resultSet.getString("last_login"));
-                int roleId = resultSet.getInt("role_id");
 
-
-                user = new User(id, login, password, firstName, lastName, birthDate, email, registrationDate, Role.getById(roleId));
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        finally {
-            BasicConnectionPool.getInstance().releaseConnection(connection);
-        }
-        return user;
+    @Override
+    public List<User> readWithOffset(int offset, int num) {
+        return null;
     }
 
     @Override
     public User findById(int id) {
-        return null;
+        Connection connection = BasicConnectionPool.getInstance().getConnection();
+        User user = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM kinorating.users where id = ? limit 1"
+        )) {
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if(resultSet.next()) {
+                setParams(resultSet);
+            }
+
+            user = new User(id, login, password, firstName, lastName, birthDate, email, registrationDate, Role.getById(roleId));
+            initUserRates(user);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            BasicConnectionPool.getInstance().releaseConnection(connection);
+        }
+        return user;
     }
 
     @Override
@@ -119,6 +127,65 @@ public class UserDAO implements DataAccessObject<User> {
     @Override
     public void update(User user) {
 
+    }
+
+    public User findByLoginAndPassword(String login, String password) {
+        Connection connection = BasicConnectionPool.getInstance().getConnection();
+        User user = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM kinorating.users where login = ? and password = ?"
+        )) {
+            statement.setString(1, login);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                setParams(resultSet);
+                user = new User(id, login, password, firstName, lastName, birthDate, email, registrationDate, Role.getById(roleId));
+                initUserRates(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        finally {
+            BasicConnectionPool.getInstance().releaseConnection(connection);
+        }
+        return user;
+    }
+
+    public void initUserRates(User user) {
+        Map<Integer, Byte> rates = user.getRates();
+        Connection connection = BasicConnectionPool.getInstance().getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM kinorating.kino_ratings where user_id = ?;"
+        )) {
+            statement.setInt(1, user.getId());
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int showId = resultSet.getInt("kino_id");
+                byte rate = (byte) resultSet.getInt("kino_rating");
+                rates.put(showId, rate);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            BasicConnectionPool.getInstance().releaseConnection(connection);
+        }
+    }
+
+    private void setParams(ResultSet resultSet) throws SQLException {
+        id = resultSet.getInt("id");
+        firstName = resultSet.getString("first_name");
+        lastName = resultSet.getString("last_name");
+        login = resultSet.getString("login");
+        password = resultSet.getString("password");
+        birthDate = resultSet.getDate("birth_date").toLocalDate();
+        email = resultSet.getString("email");
+        registrationDate = resultSet.getDate("registration_date").toLocalDate();
+        roleId = resultSet.getInt("role_id");
     }
 
 }
