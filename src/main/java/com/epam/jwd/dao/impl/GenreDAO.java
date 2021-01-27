3e1bba203e84d46e5fc7315e454a732bc774e010
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-27 23:17:17 +0300

    Add genres to show method in GenreDAO

--- src/main/java/com/epam/jwd/dao/impl/GenreDAO.java
+++ src/main/java/com/epam/jwd/dao/impl/GenreDAO.java
@@ -3,11 +3,14 @@ package com.epam.jwd.dao.impl;
 import com.epam.jwd.connect.impl.BasicConnectionPool;
 import com.epam.jwd.dao.DataAccessObject;
 import com.epam.jwd.domain.Genre;
-import com.epam.jwd.domain.Movie;
+import com.epam.jwd.domain.Show;
+import com.epam.jwd.service.GenreService;
+import org.apache.log4j.Logger;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +23,8 @@ public class GenreDAO implements DataAccessObject<Genre> {
     public static GenreDAO getInstance() {
         return INSTANCE;
     }
+
+    private static final Logger logger = Logger.getLogger(GenreDAO.class);
 
     @Override
     public List<Genre> readAll() {
@@ -34,8 +39,8 @@ public class GenreDAO implements DataAccessObject<Genre> {
                 Genre genre = new Genre(id, name);
                 genres.add(genre);
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException exception) {
+            logger.error("Genres init error", exception);
         }
         finally {
             BasicConnectionPool.getInstance().releaseConnection(connection);
@@ -85,6 +90,58 @@ public class GenreDAO implements DataAccessObject<Genre> {
         finally {
             BasicConnectionPool.getInstance().releaseConnection(connection);
         }
+    }
+
+    public void addGenresToShow(Show show) throws SQLException {
+        Connection connection = BasicConnectionPool.getInstance().getConnection();
+        List<Genre> genres = show.getGenres();
+        try (PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO kinorating.kino_genres (kino_id, genre_id) VALUES (?, ?)"
+        )) {
+            connection.setAutoCommit(false);
+
+            for (Genre genre : genres) {
+                statement.setInt(1, show.getId());
+                statement.setInt(2, genre.getId());
+                statement.addBatch();
+            }
+
+            statement.executeBatch();
+
+            connection.commit();
+
+        } catch (SQLException exception) {
+            connection.rollback();
+            logger.error("Error adding genres to a show", exception);
+        }
+        finally {
+            connection.setAutoCommit(true);
+            BasicConnectionPool.getInstance().releaseConnection(connection);
+        }
+    }
+
+    public List<Genre> getGenresByShow(Show show) {
+        Connection connection = BasicConnectionPool.getInstance().getConnection();
+        List<Genre> genres = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM kinorating.kino_genres where kino_id = ?"
+        )) {
+            statement.setInt(1, show.getId());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                genres.add(GenreService.findById(resultSet.getInt("genre_id")));
+            }
+
+        } catch (SQLException throwables) {
+            logger.error("Error finding genres of the show", throwables);
+        }
+        finally {
+            BasicConnectionPool.getInstance().releaseConnection(connection);
+        }
+        return genres;
     }
 
     @Override
