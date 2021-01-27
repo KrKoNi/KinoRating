eaaa21245980c7992306cc76967474555d08b201
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-27 23:24:46 +0300

    Encode filter to set utf-8

---
+++ src/main/java/com/epam/jwd/filter/EncodeFilter.java
@@ -0,0 +1,27 @@
+package com.epam.jwd.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodeFilter", urlPatterns = "/*")
+public class EncodeFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        chain.doFilter(request, response);
+    }
+}
