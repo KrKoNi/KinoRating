e97428905a87b60bbbf604b15e431310292d946e
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-30 19:33:47 +0300

    Auth filter for rates

---
+++ src/main/java/com/epam/jwd/filter/AuthFilter.java
@@ -0,0 +1,39 @@
+package com.epam.jwd.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter(filterName = "AuthFilter", urlPatterns = "/ajax/rate")
+public class AuthFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpSession session = httpServletRequest.getSession(false);
+        String loginURI = httpServletRequest.getContextPath() + "/app/login";
+
+        boolean loggedIn = session != null && session.getAttribute("userDTO") != null;
+        boolean loginRequest = httpServletRequest.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            chain.doFilter(httpServletRequest, httpServletResponse);
+        } else {
+            PrintWriter out = response.getWriter();
+            out.print("You have to be authorized to be able to rate");
+            out.flush();
+        }
+    }
+}
