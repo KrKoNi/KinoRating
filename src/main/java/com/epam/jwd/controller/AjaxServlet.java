cad898b07a3ef11e8838f1a0f96aa4383594f727
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-30 19:17:04 +0300

    Ajax servlet, controller actions and factory method for controller actions

--- src/main/java/com/epam/jwd/controller/AjaxServlet.java
+++ src/main/java/com/epam/jwd/controller/AjaxServlet.java
@@ -1,11 +1,15 @@
 package com.epam.jwd.controller;
 
+import com.epam.jwd.controller.strategy.ControllerAction;
+import com.epam.jwd.controller.strategy.factory.ajax.AjaxControllerActionFactoryMethod;
 import com.epam.jwd.dao.impl.MovieDAO;
 import com.epam.jwd.dao.impl.ShowDAO;
 import com.epam.jwd.dao.impl.TVSeriesDAO;
+import com.epam.jwd.dao.impl.UserDAO;
 import com.epam.jwd.domain.Movie;
 import com.epam.jwd.domain.Show;
 import com.epam.jwd.domain.TVSeries;
+import com.epam.jwd.dto.impl.UserDTO;
 import com.epam.jwd.service.MovieService;
 import com.epam.jwd.service.ShowService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,47 +20,32 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Locale;
 
-@WebServlet(name = "AjaxServlet", urlPatterns = "/ajax")
+@WebServlet(name = "AjaxServlet", urlPatterns = "/ajax/*")
 public class AjaxServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        service(request, response);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        service(request, response);
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String command = request.getParameter("command");
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        switch (command) {
-            case "movies":
-                List<Movie> movieList = MovieService.getInstance().readWithOffset(0, 20);
-                //List<Movie> movieList = MovieDAO.getInstance().readWithOffset(0, 20);
-                out.print(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(movieList));
-                out.flush();
-                break;
-            case "tv":
-                List<TVSeries> tvSeries = TVSeriesDAO.getInstance().readWithOffset(0,20);
-                out.print(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(tvSeries));
-                out.flush();
-                break;
-            case "search":
-                String str = request.getParameter("str");
-                List<Show> showList = ShowService.getInstance().findLike(str);
-                out.print(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(showList));
-                out.flush();
-                break;
-            default:
-                break;
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            ControllerAction action = AjaxControllerActionFactoryMethod.getAction(request);
+            action.execute(request, response);
+
+            super.service(request, response);
+
+        }
+        catch (Exception e) {
+            throw new ServletException("Executing action failed.", e);
         }
     }
 }
---
+++ src/main/java/com/epam/jwd/controller/strategy/impl/ajax/AjaxSearchControllerAction.java
@@ -0,0 +1,26 @@
+package com.epam.jwd.controller.strategy.impl.ajax;
+
+import com.epam.jwd.controller.strategy.ControllerAction;
+import com.epam.jwd.domain.Show;
+import com.epam.jwd.service.ShowService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class AjaxSearchControllerAction implements ControllerAction {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        PrintWriter out = response.getWriter();
+        String str = request.getParameter("str");
+        List<Show> showList = ShowService.findLike(str);
+        out.print(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(showList));
+        out.flush();
+
+        return "ajax";
+    }
+}
