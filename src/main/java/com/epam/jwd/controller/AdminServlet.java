4db30e58a61e11bdae74989f4ffe8c37aebedf13
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2021-01-31 02:41:40 +0300

    Admin servlet fix (post -> redirect, get -> forward)

--- src/main/java/com/epam/jwd/controller/AdminServlet.java
+++ src/main/java/com/epam/jwd/controller/AdminServlet.java
@@ -1,36 +1,36 @@
 package com.epam.jwd.controller;
 
 import com.epam.jwd.controller.strategy.ControllerAction;
-import com.epam.jwd.controller.strategy.factory.ActionFactoryMethod;
 import com.epam.jwd.controller.strategy.factory.admin.AdminControllerActionFactoryMethod;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "AdminServlet", urlPatterns = {"/admin", "/admin/*"})
 public class AdminServlet extends HttpServlet {
+    String view;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/WEB-INF/jsp/admin/" + view + ".jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect(request.getContextPath() + "/admin/" + view);
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            response.setContentType("text/html");
-            response.setCharacterEncoding("utf-8");
-
             ControllerAction action = AdminControllerActionFactoryMethod.getAction(request);
-            String view = action.execute(request, response);
+            view = action.execute(request, response);
 
-            request.getRequestDispatcher("/WEB-INF/jsp/admin/" + view + ".jsp").forward(request, response);
+            super.service(request, response);
 
         }
         catch (Exception e) {
